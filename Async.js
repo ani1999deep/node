c3b218793091a83dd350602651d95f8c504680f7
ani1999deep <anideepbhowmick1@gmail.com>
2023-04-21 17:59:44 +0530

    Update new file

---
+++ Async.js
@@ -0,0 +1,39 @@
+const fs=require('fs');
+const path=require('path');
+const dirPath=path.join(__dirname,'crud');
+const filePath=`${dirPath}/apple.txt`
+
+// Read this file
+// fs.writeFileSync(filePath,'This is async file');
+
+
+
+
+
+
+//Write this file
+// fs.readFile(filePath,"utf8",(err,item)=>{
+    
+//     console.log(item)
+// });
+
+
+
+
+
+//Update File
+// fs.appendFile(filePath,'And This is another file',(err)=>{
+
+//     if(!err) console.log('File is updated')
+// });
+
+
+
+
+//Rename of the file
+// fs.rename(filePath,`${dirPath}/appleFile.txt`,(err)=>{
+//     if(!err) console.log('File name is updated')
+// })
+
+// Delete the file
+fs.unlinkSync(`${dirPath}/appleFile.txt`)
--- displayFile.js
+++ displayFile.js
@@ -1,2 +1,26 @@
 const fs=require('fs');
-fs.writeFileSync('test.txt','This is a text file');
+const path=require('path');
+const dirPath=path.join(__dirname,'files');
+
+//Multiple file Creation
+
+// for(i=0;i<5;i++){
+
+//     fs.writeFileSync(dirPath+"/test "+i+".txt", 'There is file creation')
+// }
+
+// Read with List
+// fs.readdir(dirPath,(err,files)=>{
+   
+//     console.log(files)
+// });
+
+
+//Read with List with single file
+
+fs.readdir(dirPath,(err,files)=>{
+
+    files.forEach(element => {
+        console.log(element);
+      });
+})
