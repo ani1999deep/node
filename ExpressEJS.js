6d629e1814b9c10a71b0da900093bfc53dc99caf
ani1999deep <anideepbhowmick1@gmail.com>
2023-04-26 18:40:28 +0530

    Add new file

---
+++ ExpressEJS.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, 'public')
+
+// View engine setup
+app.set('view engine', 'ejs');
+app.get('', (_, resp) => {
+
+    resp.sendFile(`${publicPath}/index.html`)
+})
+
+app.get('/about', (_, resp) => {
+
+    resp.sendFile(`${publicPath}/about.html`)
+})
+
+app.get('/home', (_, resp) => {
+
+    resp.sendFile(`${publicPath}/home.html`)
+})
+
+app.get('/profile', (_, resp) => {
+    const user={
+        name:'Ani',
+        email:'ani@gmail.com',
+        city:'kolkata'
+    }
+
+ // Rendering home.ejs page
+ resp.render('profile',{
+    user: user
+});
+})
+
+app.get('*', (_, resp) => {
+
+    resp.sendFile(`${publicPath}/nopage.html`)
+})
+app.listen(5000, () => console.log("port is open in 5000"));
